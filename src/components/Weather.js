2be927be46d7daf6c71b34ac8b70165a7344bb80
Taylor Martin <tayloredwinmartin@gmail.com>
2020-05-28 15:30:53 -0400

    refactoring

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,18 +4,19 @@ import { DateTime } from "luxon";
 export const Weather = ({ data }) => {
   const time = DateTime.local().toLocaleString(DateTime.TIME_SIMPLE);
   const dayOfWeek = DateTime.local().weekdayLong;
+  const city = data.name;
+  const conditions = data.weather[0].main;
+  const temp = Math.floor(data.main.temp);
+  const imgUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
   return (
     <div>
-      <div>{data.name}</div>
+      <div>{city}</div>
       <div>{dayOfWeek}</div>
       <div>{time}</div>
-      <div>{data.weather[0].main}</div>
-      <div>{Math.floor(data.main.temp)}&deg;F</div>
-      <img
-        src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-        alt="No image available"
-      />
+      <div>{conditions}</div>
+      <div>{temp}&deg;F</div>
+      <img src={imgUrl} alt="No image available" />
     </div>
   );
 };
