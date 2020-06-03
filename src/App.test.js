aea85be9ae3363d672a02c0491840f335eb3f0bb
Taylor Martin <tayloredwinmartin@gmail.com>
2020-06-02 22:55:25 -0400

    added some specs

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import * as apiService from "./services/apiService";
+import { WEATHER_DATA } from "./mockWeatherData";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("app main page", () => {
+  it("renders the main feed", () => {
+    const useWeatherData = jest.spyOn(apiService, "useWeatherData");
+    useWeatherData.mockImplementation(() => {
+      return { loading: false, value: WEATHER_DATA };
+    });
+    const { getByText } = render(<App />);
+    const blogTitle = getByText("Atlanta, GA");
+    expect(blogTitle).toBeInTheDocument();
+    useWeatherData.mockRestore();
+  });
+
+  it("renders a loading state", () => {
+    const useWeatherData = jest.spyOn(apiService, "useWeatherData");
+    useWeatherData.mockImplementation(() => {
+      return { loading: true, value: undefined };
+    });
+    const { getByText } = render(<App />);
+    const loading = getByText(/loading/);
+    expect(loading).toBeInTheDocument();
+    useWeatherData.mockRestore();
+  });
+
+  it("renders an error state", () => {
+    const useWeatherData = jest.spyOn(apiService, "useWeatherData");
+    useWeatherData.mockImplementation(() => {
+      return { loading: false, value: { error: true, message: "Error" } };
+    });
+    const { getByText } = render(<App />);
+    const errorText = getByText("Error");
+    expect(errorText).toBeInTheDocument();
+    useWeatherData.mockRestore();
+  });
 });
---
+++ src/mockWeatherData.js
@@ -0,0 +1,7 @@
+export const WEATHER_DATA = {
+  data: {
+    weather: [{ main: "Cloudy", icon: "7d5" }],
+    main: { temp: 85 },
+  },
+  location: { city: "Atlanta", state_code: "GA" },
+};
